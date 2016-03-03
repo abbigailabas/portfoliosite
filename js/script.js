13393b0c4237aeee2b29507b0191ab761974e851
Abby <abbigail.abas@gmail.com>
2016-03-03 17:33:01 -0500

    yeah eh

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ var waypoint = new Waypoint({
   element: document.getElementById('about'),
   handler: function(direction) {
   	console.log(direction);
-  	$('.main-heroBG').toggleClass('heroBG2', direction === "down");
-    
+  	// $('.main-heroBG').toggleClass('heroBG2', direction === "down");
+    if (direction === "down") {
+		$('.main-heroBG').addClass('heroBG2');    	
+    } else if (direction === "up") {
+    	$('.main-heroBG').removeClass('heroBG2');
+    }
   }
 })
 
@@ -11,8 +15,20 @@ var waypoint = new Waypoint({
   element: document.getElementById('work'),
   handler: function(direction) {
   	console.log(direction);
-  	$('.main-heroBG').toggleClass('heroBG3', direction === "down");
-    
+  	// $('.main-heroBG').toggleClass('heroBG3', direction === "down");
+    if (direction === "down") {
+		$('.main-heroBG').removeClass('heroBG2')
+		setTimeout(
+    		function(){
+    			$('.main-heroBG').addClass('heroBG3');
+    		}, 1 );    	
+    } else if (direction === "up") {
+    	$('.main-heroBG').removeClass('heroBG3');
+    	setTimeout(
+    		function(){
+    			$('.main-heroBG').addClass('heroBG2');
+    		}, 1 );
+    }
   }
 })
 
